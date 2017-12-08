d4817fb15a1babacc9679ee33d7a87b82257e207
LYAVALE95 <valesolo95@hotmail.com>
2017-12-07 21:31:42 -0600

    MAPA 2

---
+++ EVA/app/assets/javascripts/locationByCoord.js
@@ -0,0 +1,63 @@
+function initMap() {
+  console.log("Hi");
+        var map = new google.maps.Map(document.getElementById('mapitapc'), {
+          zoom: 8,
+          center: {lat: 40.731, lng: -73.997}
+        });
+        var geocoder = new google.maps.Geocoder;
+        var infowindow = new google.maps.InfoWindow;
+
+        document.getElementById('submit').addEventListener('click', function() {
+          geocodeLatLng(geocoder, map, infowindow);
+        });
+      }
+      
+      function geocodeLatLng(geocoder, map, infowindow) {
+        var input = document.getElementById('latlng').text();
+        console.log("input: " + input);
+        var latlngStr = input.split(',', 2);
+        var latlng = {lat: parseFloat(latlngStr[0]), lng: parseFloat(latlngStr[1])};
+        geocoder.geocode({'location': latlng}, function(results, status) {
+          if (status === 'OK') {
+            if (results[1]) {
+              map.setZoom(11);
+              var marker = new google.maps.Marker({
+                position: latlng,
+                map: map
+              });
+              infowindow.setContent(results[1].formatted_address);
+              infowindow.open(map, marker);
+            } else {
+              window.alert('No results found');
+            }
+          } else {
+            window.alert('Geocoder failed due to: ' + status);
+          }
+        });
+      }
+/*function geocodeLatLng(coords, map) {
+
+      var geocoder = new google.maps.Geocoder;
+   var infowindow = new google.maps.InfoWindow;
+       // var input = document.getElementById('latlng').value;
+       var input = coords;
+        var latlngStr = input.split(',', 2);
+        var latlng = {lat: parseFloat(latlngStr[0]), lng: parseFloat(latlngStr[1])};
+        geocoder.geocode({'location': latlng}, function(results, status) {
+          if (status === 'OK') {
+            if (results[1]) {
+              map.setZoom(11);
+              var marker = new google.maps.Marker({
+                position: latlng,
+                map: map
+              });
+              infowindow.setContent(results[1].formatted_address);
+              infowindow.open(map, marker);
+            } else {
+              window.alert('No results found');
+            }
+          } else {
+            window.alert('Geocoder failed due to: ' + status);
+          }
+        });
+      }*/
--- EVA/app/assets/javascripts/locationsByDir.js
+++ EVA/app/assets/javascripts/locationsByDir.js
@@ -2,11 +2,19 @@
       // parameter when you first load the API. For example:
       // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
 
-      function initMap() {
+      function initMap() { 
         var map = new google.maps.Map(document.getElementById('mapitap'), {
           center: {lat: -33.8688, lng: 151.2195},
           zoom: 13
         });
+        
+        var geocoder = new google.maps.Geocoder;
+        var infowindow = new google.maps.InfoWindow;
+      if($('#submitmapita').val()){
+        document.getElementById('submitmapita').addEventListener('click', function() {
+          console.log("L");
+          geocodeLatLng(geocoder, map, infowindow);
+        });}
         var input = /** @type {!HTMLInputElement} */(
             document.getElementById('pac-inputmap'));
 
@@ -132,4 +140,37 @@
         setupClickListener('changetype-establishment', ['establishment']);
         setupClickListener('changetype-geocode', ['geocode']);*/
 
+
+        /*--------------BY COORD-----------------------------*/
+          function geocodeLatLng() {
+              var map = new google.maps.Map(document.getElementById('mapitap'), {
+          center: {lat: -33.8688, lng: 151.2195},
+          zoom: 13
+        });
+            var geocoder = new google.maps.Geocoder;
+        var infowindow = new google.maps.InfoWindow;
+
+        var input = document.getElementById('latlngmap').value;
+        //var input = coords.value;
+        console.log("input: " + input);
+        var latlngStr = input.split(',', 2);
+        var latlng = {lat: parseFloat(latlngStr[0]), lng: parseFloat(latlngStr[1])};
+        geocoder.geocode({'location': latlng}, function(results, status) {
+          if (status === 'OK') {
+            if (results[1]) {
+              map.setZoom(11);
+              var marker = new google.maps.Marker({
+                position: latlng,
+                map: map
+              });
+              infowindow.setContent(results[1].formatted_address);
+              infowindow.open(map, marker);
+            } else {
+              window.alert('No results found');
+            }
+          } else {
+            window.alert('Geocoder failed due to: ' + status);
+          }
+        });
+      }
        }
